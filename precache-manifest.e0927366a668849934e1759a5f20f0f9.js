872c5f31519fe9985be4d858cdf37b8ffb9a02fd
peven <eveha94@gmail.com>
2020-11-02 17:40:24 +0100

    Updates

---
+++ precache-manifest.e0927366a668849934e1759a5f20f0f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ee025fdee3584cef36f10cdb21ebd0",
+    "url": "/cicd-react-tut/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/cicd-react-tut/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "7ef9ce297d4db33e64a7",
+    "url": "/cicd-react-tut/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/cicd-react-tut/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cicd-react-tut/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef9ce297d4db33e64a7",
+    "url": "/cicd-react-tut/static/js/main.eb61fb98.chunk.js"
+  },
+  {
+    "revision": "bd6f29e2e8da64ba5e20",
+    "url": "/cicd-react-tut/static/js/runtime-main.25a7d773.js"
+  }
+]);
